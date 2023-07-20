7def72ff4390f3c485543a5afad181668a966f13
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-20 13:24:36 +0100

    Initial commit

---
+++ android/app/src/main/java/com/eshop/vendor/vendor_eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package swipecart.seller;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Flutter_version\flutter_windows_3.0.5-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\M-Mart_\M_mart_seller\M_mart_Seller"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
